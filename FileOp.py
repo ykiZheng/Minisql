f6409f3ee5d00d4563d5089a9748238c456a8bb1
ykiZheng <3190102363@zju.edu.cn>
2021-06-10 16:03:05 +0800

    :bug:fix: 修改文件编码方式为utf-8

--- FileOp.py
+++ FileOp.py
@@ -7,29 +7,35 @@ import time
 import json
 import os
 
-
-DBFiles = 'DBFiles/{}.json' # 表路径
+DBFiles = 'DBFiles/{}.json'  # 表路径
 index_File = 'index/{}_index.json'  # 索引路径
-log_file = 'log.txt'    #log路径
-localtime = time.asctime(time.localtime(time.time())) #当前时间
+log_file = 'log.txt'  # log路径
+localtime = time.asctime(time.localtime(time.time()))  # 当前时间
 
 # 异常类型
+
+
 class MiniSQLError(Exception):
     pass
+
 
 class MiniSQLSyntaxError(MiniSQLError):
     pass
 
 # log 信息保存
+
+
 def log(msg):
     print(msg)
-    with open(log_file, 'a',encoding='utf-8') as f:
+    with open(log_file, 'a', encoding='utf-8') as f:
         f.write(localtime+': '+msg+'\n')
 
 # json格式导入
+
+
 def load(path):
     try:
-        with open(path, 'r',encoding='utf-8') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             if os.path.getsize(path):
                 data = json.load(f)
             else:
@@ -39,9 +45,11 @@ def load(path):
         return []
 
 # json格式保存
+
+
 def store(obj, path):
     print(obj)
-    with open(path, 'w',encoding='utf-8') as fw:
+    with open(path, 'w', encoding='utf-8') as fw:
         json.dump(obj, fw)
 
 
@@ -49,6 +57,7 @@ def main():
     path = "DBFiles\myDB.json"
     schemas = load(path)
     print(schemas)
+
 
 if __name__ == "__main__":
     main()
