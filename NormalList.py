1109eea0496e6a3106d9ec4bf706681fd0d4a8eb
ytm <74521736+cmather-s@users.noreply.github.com>
2021-06-14 13:24:34 +0800

    Bottom level1.1
    Fix where condition

--- NormalList.py
+++ NormalList.py
@@ -8,23 +8,59 @@ class NormalList():
         self.keys = list['keys']
         self.values = list['values']
 
-    def Search_key(self, key):
+    def Condition_search_node(self, key, condition):
         res = [False]
+        temp = []
         for i in range(len(self.keys)):
-            if key == self.keys[i]:
-                res[0] = True
-                res.append(self.values[i])
-                break
+            if condition == 0:
+                if self.keys[i] == key:
+                    temp.append(self.values[i])
+            elif condition == 1:
+                if self.keys[i] != key:
+                    temp.append(self.values[i])
+            elif condition == 2:
+                if self.keys[i] < key:
+                    temp.append(self.values[i])
+            elif condition == 3:
+                if self.keys[i] > key:
+                    temp.append(self.values[i])
+            elif condition == 4:
+                if self.keys[i] <= key:
+                    temp.append(self.values[i])
+            elif condition == 5:
+                if self.keys[i] >= key:
+                    temp.append(self.values[i])
+        if temp:
+            res.append(temp)
         return res
 
     def Insert_node(self, key, value):
         self.keys.append(key)
         self.values.append(value)
 
-    def Delete_key(self, key):
+    def Condition_delete_node(self, key, condition):
         for i in range(len(self.keys)):
-            if key == self.keys[i]:
-                self.keys.pop(i)
-                self.values.pop(i)
-                return True
-        return False
+            if condition == 0:
+                if self.keys[i] == key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
+            elif condition == 1:
+                if self.keys[i] != key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
+            elif condition == 2:
+                if self.keys[i] < key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
+            elif condition == 3:
+                if self.keys[i] > key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
+            elif condition == 4:
+                if self.keys[i] <= key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
+            elif condition == 5:
+                if self.keys[i] >= key:
+                    self.keys.pop(i)
+                    self.values.pop(i)
--- index.py
+++ index.py
@@ -4,23 +4,18 @@ from buffer import Buffer
 import json
 import os
 import struct as st
-from FileOp import *
 
-def Initialize(indexpath, listpath, datapath):
-    # indexpath = index_filepath.format(DBName__)
-    # listpath= list_filepath.format(DBName__)
-    # datapath = data_filepath.format(DBName__)
-    fp_index = open(indexpath, 'w')
-    fp_list = open(listpath, 'w')
-    
-    if not os.path.getsize(indexpath):
+def Initialize(index_filepath, list_filepath, data_filepath):
+    fp_index = open(index_filepath, 'a')
+    fp_list = open(list_filepath, 'a')
+    if not os.path.getsize(index_filepath):
         a = {}
         fp_index.write(json.dumps(a))
-    if not os.path.getsize(listpath):
+    if not os.path.getsize(list_filepath):
         a = {}
         fp_list.write(json.dumps(a))
-    fp_data = open(datapath, 'wb')
-    if not os.path.getsize(datapath):
+    fp_data = open(data_filepath, 'rb+')
+    if not os.path.getsize(data_filepath):
         a = 1024
         b = 0
         a = st.pack('i', a)
@@ -43,10 +38,10 @@ class Index():
         Initialize(index_filepath, list_filepath, data_filepath)
         self.index_filepath = index_filepath
         self.list_filepath = list_filepath
-        with open(self.index_filepath,'r') as Ifp:
+        with open(index_filepath,'r') as Ifp:
             self.index_trees = json.load(Ifp)
             Ifp.close()
-        with open(self.list_filepath,'r') as Lfp:
+        with open(list_filepath,'r') as Lfp:
             self.normal_list = json.load(Lfp)
             Lfp.close()
         self.buffer.Load_data_file(data_filepath)
@@ -77,46 +72,46 @@ class Index():
     def Drop_table(self, table_name, primary_key):
         BT = BPlusTree()
         BT.Trees = self.index_trees[table_name][primary_key]
-        BT.Fetch_all_nodes(BT.Trees)
-        all_data_offset = BT.all_values
+        all_data_offset = BT.Fetch_all_nodes()
         for it in all_data_offset:
             self.buffer.Delete_data(it)
         self.Drop_field_from_table
         self.index_trees.pop(table_name)
         self.normal_list.pop(table_name)
 
-    def Create_index(self, table_name, column_name):
-        BT = BPlusTree()
-        BT.BuildNewBPTree()
-        keys = self.normal_list[table_name][column_name]['keys']
-        values = self.normal_list[table_name][column_name]['values']
-        for i in range(len(keys)):
-            BT.Insert_node(keys[i], values[i])
-        self.index_trees[table_name][column_name] = BT.Trees
-        self.normal_list[table_name].pop(column_name)
+    # def Create_index(self, table_name, column_name):
+    #     BT = BPlusTree()
+    #     BT.BuildNewBPTree()
+    #     keys = self.normal_list[table_name][column_name]['keys']
+    #     values = self.normal_list[table_name][column_name]['values']
+    #     for i in range(len(keys)):
+    #         BT.Insert_node(keys[i], values[i])
+    #     self.index_trees[table_name][column_name] = BT.Trees
+    #     self.normal_list[table_name].pop(column_name)
 
-    def Delete_index(self, table_name, column_name):
-        pass
+    # def Delete_index(self, table_name, column_name):
+    #     pass
 
-    def Select_from_table(self, table_name, column_name, key, attribute, isindex):
+    def Select_from_table(self, table_name, column_name, key, attribute, isindex, condition):
+        data_res = []
         if isindex:
             BT = BPlusTree()
             BT.Trees = self.index_trees[table_name][column_name]
-            res = BT.Search_key(key)
+            res = BT.Condition_search_node(key, condition)
             if res[0]:
-                offset = res[1]
-                data_res = self.buffer.Search_data(offset, attribute)
+                data_offsets = res[1]
+                for offset in data_offsets:
+                    data_res.append(self.buffer.Search_data(offset, attribute))
                 res[1] = data_res
             return res
         else:
             NL = NormalList()
-            if column_name == []:
-                return None
             NL.Load_list(self.normal_list[table_name][column_name])
-            res = NL.Search_key(key)
+            res = NL.Condition_search_node(key, condition)
             if res[0]:
-                offset = res[1]
-                data_res = self.buffer.Search_data(offset, attribute)
+                data_offsets = res[1]
+                for offset in data_offsets:
+                    data_res.append(self.buffer.Search_data(offset))
                 res[1] = data_res
             return res
 
@@ -133,39 +128,46 @@ class Index():
                 NL.Load_list(self.normal_list[table_name][column[i]])
                 NL.Insert_node(values[i], offset)
 
-    def Drop_field_from_table(self, table_name, column, column_name, key, isindex, index_list):
+    def Drop_field_from_table(self, table_name, column, column_name, key, isindex, index_list, condition):
+        data_fields = []
         if isindex:
             BT = BPlusTree()
             BT.Trees = self.index_trees[table_name][column_name]
-            res = BT.Search_key(key)
+            res = BT.Condition_search_node(key)
             if res[0]:
-                offset = res[1]
-                BT.Delete_key(key)
-                return True
+                data_offsets = res[1]
+                for offset in data_offsets:
+                    data_fields.append(self.buffer.Search_data(offset))
+                    self.buffer.Delete_data(offset)
+                BT.Condition_delete_node(key, condition)
             else:
                 return False
         else:
             NL = NormalList()
             NL.Load_list(self.normal_list[table_name][column_name])
-            res = NL.Search_key(key)
+            res = NL.Condition_search_node(key, condition)
             if res[0]:
-                offset = res[1]
-                NL.Delete_key(key)
+                data_offsets = res[1]
+                for offset in data_offsets:
+                    data_fields.append(self.buffer.Search_data(offset))
+                    self.buffer.Delete_data(offset)
+                NL.Condition_delete_node(key, condition)
             else:
                 return False
-        field_values = self.buffer.Search_data(offset)
-        self.buffer.Delete_data(offset)
+
         for i in range(len(index_list)):
             if column[i] != column_name:
                 if index_list[i]:
                     BT = BPlusTree()
                     BT.Trees = self.index_trees[table_name][column[i]]
-                    BT.Delete_key(field_values[i])
+                    for item in data_fields:
+                        BT.Delete_key(item[i])
                 else:
                     NL = NormalList()
                     NL.Load_list(self.normal_list[table_name][column[i]])
-                    NL.Delete_key(key)
-
+                    for item in data_fields:
+                        NL.Delete_key(item[i])
+        return True
 
     def Update_field_from_table(self, table_name, column, column_name, key, isindex, attribute, values, index_list):
         res = self.Drop_field_from_table(table_name, column, column_name, key, isindex, index_list)
@@ -173,11 +175,3 @@ class Index():
             return False
         self.Insert_into_table(table_name, column, attribute, values, index_list)
         return True
-
-
-def main():
-    # globalValue.currentDB = "myDB"
-    index = Index()
-
-if __name__ == '__main__':
-    main()
